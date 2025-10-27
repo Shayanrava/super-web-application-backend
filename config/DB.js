32f081b390b123c0d6bfcfd27dc8598b2bba74f9
Shayanrava <shayanravakhah57@gmail.com>
2025-10-27 11:57:27 +0330

    repair 502 error

--- config/DB.js
+++ config/DB.js
@@ -1,13 +1,37 @@
 import { Sequelize } from "sequelize";
 
 const db = new Sequelize(
-  "super_web_app",
-  "root",
-  "Shayan$$%%",
+  process.env.MYSQL_DATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQL_ROOT_PASSWORD,
   {
-    host: "localhost",
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
     dialect: "mysql",
   }
 );
 export default db;
+
+
+// const db = new Sequelize(
+//   "super_web_app",
+//   "root",
+//   "Shayan$$%%",
+//   {
+//     host: "localhost",
+//     dialect: "mysql",
+//   }
+// );
+
+
+// const db = new Sequelize(
+//   process.env.MYSQL_DATABASE,
+//   process.env.MYSQLUSER,
+//   process.env.MYSQL_ROOT_PASSWORD,
+//   {
+//     host: process.env.MYSQLHOST,
+//     port: process.env.MYSQLPORT,
+//     dialect: "mysql",
+//   }
+// );
 
